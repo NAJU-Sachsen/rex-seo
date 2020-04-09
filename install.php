9681ae6687e9f2f5aa921678e4d6acc1ae670502
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-04-09 09:54:29 +0200

    Create initial package structure

---
+++ install.php
@@ -0,0 +1,20 @@
+<?php
+
+$seo_table = rex_sql_table::get('naju_seo');
+
+if ($seo_table->exists()) {
+    // create meta description if necessary
+    $sql = rex_sql::factory()->setQuery('select meta_value from naju_seo where meta_key = :key', ['key' => 'description']);
+
+    if (!$sql->getRows()) {
+        rex_sql::factory()->setQuery('insert into naju_seo (meta_key, meta_value) values (:key, :val)', ['key' => 'description', 'val' => '']);
+    }
+} else {
+    $seo_table
+        ->addColumn(new rex_sql_column('meta_key', 'varchar(30)'))
+        ->addColumn(new rex_sql_column('meta_value', 'mediumtext'))
+        ->setPrimaryKey('meta_key')
+        ->create();
+
+    rex_sql::factory()->setQuery('insert into naju_seo (meta_key, meta_value) values (:key, :val)', ['key' => 'description', 'val' => '']);
+}
---
+++ pages/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$addon = rex_addon::get('naju_seo');
+
+echo rex_view::title('SEO');
